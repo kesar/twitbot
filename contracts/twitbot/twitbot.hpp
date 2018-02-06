2d67fba077436ce3ea32c0789071d21f4f3d75b8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-06 13:33:45 -0300

    using EOSLIB_SERIALIZE

--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -5,6 +5,9 @@
 #include <eosiolib/dispatcher.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/string.hpp>
+#include <eosiolib/token.hpp>
+#include <eosiolib/db.hpp>
+#include <eosiolib/native_currency.hpp>
 
 using namespace eosio;
 
@@ -12,14 +15,15 @@ using namespace eosio;
 namespace twitbot {
 class contract {
 public:
-    ACTION( N(eosio), transfer ) {
-        account_name from;
-        account_name to;
-        uint64_t     quantity;
-        string       memo;
+    typedef native_currency::transfer transfer;
+//    ACTION( N(eosio), transfer ) {
+//        account_name from;
+//        account_name to;
+//        uint64_t     quantity;
+//        string       memo;
 
-        EOSLIB_SERIALIZE(transfer, (from)(to)(quantity)(memo))
-    };
+//        EOSLIB_SERIALIZE(transfer, (from)(to)(quantity)(memo))
+//    };
 
     static void on( const transfer& act ) {
         eosio::print("and the memo is ... ", act.memo, "\n");
