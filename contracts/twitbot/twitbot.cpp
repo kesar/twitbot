e490b19dd52a398be8f5d746211ccc0b99183840
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-06 13:08:43 -0300

    memo is printed

--- contracts/twitbot/twitbot.cpp
+++ contracts/twitbot/twitbot.cpp
@@ -2,64 +2,7 @@
 
 extern "C" {
 void apply( uint64_t code, uint64_t action ) {
-   twitbot::contract::apply( code, action );
+    eosio::print("account: ", eosio::name(code), ", act: ", eosio::name(action), "\n");
+    eosio::dispatch<twitbot::contract, twitbot::contract::transfer>(code,action);
 }
-}
-
-namespace TWITBOT {
-//    using namespace eosio;
-
-//    void apply_transfer(const eosio::transfer &msg) {
-//        // TODO: get memo (to know the account)
-//        // TODO: create database of twitter account -> amount
-//        print("got into apply transfer: ", msg.quantity," ", "\n");
-//    }
-
-//    void apply_withdrawal(const TWITBOT::withdrawal &msg) {
-//        // TODO: check that this is called by twitbot account
-//        // TODO: send amount to owner
-//        print("got into withdrawal: ", msg.quantity, "\n");
-//    }
-
-//    void apply_tipping(const TWITBOT::tipping &msg) {
-//        // TODO: check that this is called by twitbot account
-//        // TODO: check that from has balance and send it to destinationPW5J2gJSFPR5rRCQ92xsf4MkfYqoUKPPGg9LXycB81j6jxjmq8mtU
-//        print("got into apply tipping: ", msg.quantity, "\n");
-//    }
-//}
-
-
-//using namespace TWITBOT;
-
-//extern "C" {
-//    void init() {
-//        print("[TWITBOT contract] intializing\n");
-//    }
-
-//    void apply(uint64_t code, uint64_t action) {
-//        print("[TWITBOT contract] received ", name(code), "->", name(action), "\n");
-
-//        if (code == N(eos) && action == N(transfer)) {
-//            print("Transfer received!\n");
-//            //print(current_message);
-//            apply_transfer(current_message<eosio::transfer>());
-//            // assert(false, "rejecting transaction!"); // How to throw an exception
-//        }
-
-//        if (code != N(twitbot))
-//            return;
-
-//        switch (action) {
-//            case N(withdrawal):
-//                print("withdrawaling ...!\n");
-//                apply_withdrawal(current_message<withdrawal>());
-//                break;
-//            case N(tip):
-//                print("tipping ...!\n");
-//                apply_tipping(current_message<tipping>());
-//                break;
-//            default:
-//                break;
-//        }
-//    }
 }
--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -9,47 +9,37 @@
 using namespace eosio;
 
 
-
 namespace twitbot {
 class contract {
 public:
-    static void apply( account_name code, action_name action) {
-        //eosio::dispatch<generic_currency, transfer_memo, issue>(c,act);
-        eosio::print("account: ", eosio::name(code), ", act: ", eosio::name(action), "\n");
+    ACTION( N(eosio), transfer ) {
+        account_name from;
+        account_name to;
+        uint64_t     quantity;
+        string       memo;
 
-        if (code == N(eosio) && action == N(transfer)) {
-            eosio::print("Transfer received!\n");
+        template<typename DataStream>
+        friend DataStream& operator << ( DataStream& ds, const transfer& t ){
+            return ds << t.from << t.to << t.quantity << t.memo;
         }
-        else
-            eosio::print("not received\n");
+
+        template<typename DataStream>
+        friend DataStream& operator >> ( DataStream& ds,  transfer& t ){
+            return ds >> t.from >> t.to >> t.quantity >> t.memo;
+
+        }
+    };
+
+    static void on( const transfer& act ) {
+        eosio::print("and the memo is ... ", act.memo, "\n");
     }
-
-//    ACTION( code, transfer ) {
-//          account_name from;
-//          account_name to;
-//          uint64_t     quantity;
-//          string       memo;
-
-//          template<typename DataStream>
-//          friend DataStream& operator << ( DataStream& ds, const transfer& t ){
-//             return ds << t.from << t.to << t.quantity << t.memo;
-//          }
-
-//          template<typename DataStream>
-//          friend DataStream& operator >> ( DataStream& ds,  transfer& t ){
-//             return ds >> t.from >> t.to >> t.quantity >> t.memo;
-
-//          }
-
-//       };
-//    };
 };
+}
 
 
 
 
 
-} // namespace
 
 ///**
 // *  @abi action
