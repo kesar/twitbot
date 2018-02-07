51fe04f9aa2cf68223288ffbd631b6d67b14e275
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-07 11:16:28 +0100

    added table accounts

--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -3,14 +3,28 @@
 #include <eosiolib/eos.hpp>
 #include <eosiolib/db.hpp>
 #include <eosiolib/native_currency.hpp>
+#include <eosiolib/table.hpp>
+
+using namespace eosio;
 
 namespace twitbot {
 class contract {
 public:
-    typedef eosio::native_currency::transfer transfer;
+    /**
+     *  @abi table
+     */
+    struct account {
+       string twitter_account;
+       uint64_t balance;
+       EOSLIB_SERIALIZE( account, (twitter_account)(balance) )
+    };
+    using accounts = table<N(twitbot), N(twitbot), N(account), account, string >;
 
+    using transfer = native_currency::transfer;
     static void on( const transfer& act ) {
-        eosio::print("and the memo is ... ", act.memo, "\n");
+        print("and the memo is ... ", act.memo, "\n");
+        account a = {act.memo, act.quantity};
     }
 };
+
 }
