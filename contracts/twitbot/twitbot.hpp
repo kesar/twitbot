c1b030e3ee2c88f388f86cecfb1137cb0513abf1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-08 12:16:09 +0100

    using name as primary_key

--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -10,16 +10,15 @@ using namespace eosio;
 namespace twitbot {
 class contract {
 public:
-    name d;
     /**
      *  @abi table
      */
     struct account {
-       uint128_t twitter_account;
+       name twitter_account;
        uint64_t balance;
        EOSLIB_SERIALIZE( account, (twitter_account)(balance) )
     };
-    using accounts = table<N(twitbot), N(twitbot), N(account), account, uint128_t >;
+    using accounts = table<N(twitbot), N(twitbot), N(account), account, name >;
 
     using transfer = native_currency::transfer;
     static void on( const transfer& act ) {
