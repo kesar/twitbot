db7b110e853a78a00ea080e406f99b4a13167307
Cesar Rodriguez <kesarr@gmail.com>
2018-02-10 01:05:57 +0100

    wip

--- contracts/twitbot/twitbot.cpp
+++ contracts/twitbot/twitbot.cpp
@@ -1,8 +1,8 @@
 #include "twitbot.hpp"
 
 extern "C" {
-void apply( uint64_t code, uint64_t action ) {
+void apply(uint64_t code, uint64_t action) {
     eosio::print("account: ", eosio::name(code), ", act: ", eosio::name(action), "\n");
-    eosio::dispatch<twitbot::contract, twitbot::contract::transfer>(code,action);
+    twitbot::contract < N(twitbot) > ::apply(code, action);
 }
 }
--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -8,23 +8,53 @@
 using namespace eosio;
 
 namespace twitbot {
-class contract {
-public:
-    /**
-     *  @abi table
-     */
-    struct account {
-       name twitter_account;
-       uint64_t balance;
-       EOSLIB_SERIALIZE( account, (twitter_account)(balance) )
+    template<account_name TwitBotAccount>
+    class contract {
+    public:
+        /**
+         *  @abi table
+         */
+        struct account {
+            name twitter_account;
+            uint64_t balance;
+
+            EOSLIB_SERIALIZE(account, (twitter_account)(balance))
+        };
+
+        using accounts = table<N(twitbot), N(twitbot), N(account), account, name>;
+
+        using transfer = native_currency::transfer;
+
+        ACTION(TwitBotAccount, tip) {
+            account_name from_eos;
+            name to_twitter;
+            uint64_t amount;
+
+            EOSLIB_SERIALIZE(tip, (from_eos)(to_twitter)(amount));
+        };
+
+        ACTION(TwitBotAccount, withdraw) {
+            account_name to_eos;
+
+            EOSLIB_SERIALIZE(withdraw, (to_eos));
+        };
+
+        static void on(const transfer &transfer) {
+            eosio::print(transfer.memo);
+        };
+
+        static void on(const tip &tip) {
+            eosio::print(tip.amount);
+        };
+
+        static void on(const withdraw &withdraw) {
+            eosio::print(withdraw.to_eos);
+        };
+
+        static void apply(account_name code, action_name action) {
+            if (!eosio::dispatch<contract, transfer, tip, withdraw>(code, action)) {
+                assert(0, "received unexpected action");
+            }
+        };
     };
-    using accounts = table<N(twitbot), N(twitbot), N(account), account, name >;
-
-    using transfer = native_currency::transfer;
-    static void on( const transfer& act ) {
-        print("and the memo is ... ", act.memo, "\n");
-        //account a = {act.memo, act.quantity};
-    }
-};
-
 }
