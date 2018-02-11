a6630ecf9c363297190edcba5fdcec3df1fc9647
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-11 18:32:59 +0100

    added method to converst string -> name + put definition in cpp

--- contracts/twitbot/twitbot.cpp
+++ contracts/twitbot/twitbot.cpp
@@ -3,6 +3,90 @@
 extern "C" {
 void apply(uint64_t code, uint64_t action) {
     eosio::print("account: ", eosio::name(code), ", act: ", eosio::name(action), "\n");
-    twitbot::contract < N(twitbot) > ::apply(code, action);
+
+    if (!eosio::dispatch<
+            twitbot::contract,
+            twitbot::contract::transfer,
+            twitbot::contract::tip,
+            twitbot::contract::withdraw>(code, action)) {
+        assert(0, "received unexpected action");
+    }
 }
+} // extern
+
+void twitbot::contract::on(const twitbot::contract::transfer &transfer) {
+    eosio::print("transfering EOS to ", transfer.memo, "\n");
+    account existing_account;
+    name twitter_name = string_to_name(transfer.memo.get_data());
+    bool account_exists = accounts::get(twitter_name, existing_account);
+    if (account_exists) {
+        existing_account.balance = existing_account.balance + transfer.quantity;
+        accounts::update(existing_account);
+    } else {
+        existing_account.twitter_account = twitter_name;
+        existing_account.balance = transfer.quantity;
+        accounts::store(existing_account);
+    }
 }
+
+void twitbot::contract::on(const tip &tip) {
+    require_auth(code);
+    eosio::print("tipping EOS to ", tip.to_twitter);
+    account account_from;
+    account account_to;
+    bool account_exists_from = accounts::get(tip.from_twitter, account_from);
+    bool account_exists_to = accounts::get(tip.to_twitter, account_to);
+    assert(account_exists_from == false, "account does not exist");
+    assert(account_from.balance < tip.quantity, "account has not enough balance");
+
+    account_from.balance = account_from.balance - tip.quantity;
+    accounts::update(account_from);
+
+    if (account_exists_to) {
+        account_to.balance = account_to.balance + tip.quantity;
+        accounts::update(account_to);
+    } else {
+        account_to.twitter_account = tip.to_twitter;
+        account_to.balance = tip.quantity;
+        accounts::store(account_to);
+    }
+}
+
+void twitbot::contract::on(const withdraw &withdraw) {
+    eosio::print("withdraw to: ", withdraw.to_eos);
+    require_auth(code);
+    account existing_account;
+    bool account_exists = accounts::get(withdraw.to_eos, existing_account);
+    assert(account_exists == false, "account does not exist");
+    existing_account.balance = 0;
+    accounts::update(existing_account);
+    // TODO: send EOS
+    eosio::print("it should send: ", withdraw.to_eos);
+}
+
+uint64_t twitbot::contract::string_to_name(const char *str) {
+
+    uint32_t len = 0;
+    while( str[len] ) ++len;
+
+    uint64_t value = 0;
+
+    for( uint32_t i = 0; i <= 12; ++i ) {
+        uint64_t c = 0;
+        if( i < len && i <= 12 ) c = char_to_symbol( str[i] );
+
+        if( i < 12 ) {
+            c &= 0x1f;
+            c <<= 64-5*(i+1);
+        }
+        else {
+            c &= 0x0f;
+        }
+
+        value |= c;
+    }
+
+    return value;
+}
+
+
--- contracts/twitbot/twitbot.hpp
+++ contracts/twitbot/twitbot.hpp
@@ -8,97 +8,51 @@
 using namespace eosio;
 
 namespace twitbot {
-    template<account_name TwitBotAccount>
-    class contract {
-    public:
-        /**
+
+class contract {
+     static const uint64_t code = N(twitbot);
+
+public:
+    /**
          *  @abi table
          */
-        struct account {
-            name twitter_account;
-            uint64_t balance;
+    struct account {
+        name twitter_account;
+        uint64_t balance;
 
-            EOSLIB_SERIALIZE(account, (twitter_account)(balance))
-        };
-
-        using accounts = table<
-                N(twitbot), // scope
-                N(twitbot), // code
-                N(account), // table
-                account, // record struct
-                name // primary key
-        >;
-
-        using transfer = native_currency::transfer;
-
-        ACTION(TwitBotAccount, tip) {
-            name from_twitter;
-            name to_twitter;
-            uint64_t quantity;
-
-            EOSLIB_SERIALIZE(tip, (from_twitter)(to_twitter)(quantity));
-        };
-
-        ACTION(TwitBotAccount, withdraw) {
-            account_name to_eos;
-
-            EOSLIB_SERIALIZE(withdraw, (to_eos));
-        };
-
-        static void on(const transfer &transfer) {
-            eosio::print("transfering EOS to ", transfer.memo.get_data());
-            account existing_account;
-            name twitter_name = N(transfer.memo.get_data());
-            bool account_exists = accounts::get(twitter_name, existing_account);
-            if (account_exists) {
-                existing_account.balance = existing_account.balance + transfer.quantity;
-                accounts::update(existing_account);
-            } else {
-                existing_account.twitter_account = twitter_name;
-                existing_account.balance = transfer.quantity;
-                accounts::store(existing_account);
-            }
-        };
-
-        static void on(const tip &tip) {
-            require_auth(TwitBotAccount);
-            eosio::print("tipping EOS to ", tip.to_twitter);
-            account account_from;
-            account account_to;
-            bool account_exists_from = accounts::get(tip.from_twitter, account_from);
-            bool account_exists_to = accounts::get(tip.to_twitter, account_to);
-            assert(account_exists_from == false, "account does not exist");
-            assert(account_from.balance < tip.quantity, "account has not enough balance");
-
-            account_from.balance = account_from.balance - tip.quantity;
-            accounts::update(account_from);
-
-            if (account_exists_to) {
-                account_to.balance = account_to.balance + tip.quantity;
-                accounts::update(account_to);
-            } else {
-                account_to.twitter_account = tip.to_twitter;
-                account_to.balance = tip.quantity;
-                accounts::store(account_to);
-            }
-        };
-
-        static void on(const withdraw &withdraw) {
-            eosio::print("withdraw to: ", withdraw.to_eos);
-            require_auth(TwitBotAccount);
-            account existing_account;
-            bool account_exists = accounts::get(withdraw.to_eos, existing_account);
-            assert(account_exists == false, "account does not exist");
-            existing_account.balance = 0;
-            accounts::update(existing_account);
-            // TODO: send EOS
-            eosio::print("it should send: ", withdraw.to_eos);
-        };
-
-        static void apply(account_name code, action_name action) {
-            if (!eosio::dispatch<contract, transfer, tip, withdraw>(code, action)) {
-                assert(0, "received unexpected action");
-            }
-        };
+        EOSLIB_SERIALIZE(account, (twitter_account)(balance))
     };
-}
+
+    using accounts = table<
+        code, // scope
+        code, // code
+        N(account), // table
+        account, // record struct
+        name // primary key
+    >;
+
+    using transfer = native_currency::transfer;
+
+    ACTION(code, tip) {
+        name from_twitter;
+        name to_twitter;
+        uint64_t quantity;
+
+        EOSLIB_SERIALIZE(tip, (from_twitter)(to_twitter)(quantity));
+    };
+
+    ACTION(code, withdraw) {
+        account_name to_eos;
+
+        EOSLIB_SERIALIZE(withdraw, (to_eos));
+    };
+
+    static void on(const transfer &transfer);
+    static void on(const tip &tip);
+    static void on(const withdraw &withdraw);
+
+private:
+    static uint64_t string_to_name( const char* str );
+};
+
+} // namespace
