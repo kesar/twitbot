5c18362241e94ff3054df9bb79d0afdac3275bcc
Cesar Rodriguez <kesarr@gmail.com>
2018-04-28 21:09:58 +0200

    transfer received

--- contracts/twitbot/twitbot.cpp
+++ contracts/twitbot/twitbot.cpp
@@ -15,6 +15,14 @@ public:
 
     void withdraw(string from_twitter, account_name to_eos) {
         require_auth(_self);
+
+        // TODO: check amount
+
+        action(permission_level{_self, N(active)}, N(eosio.token), N(transfer),
+               make_tuple(_self, to_eos, 0, string(""))).send();
+
+        // TODO: remove from table amount
+
         print("withdraw");
     }
 
@@ -23,7 +31,22 @@ public:
         print("claim");
     }
 
-    //TODO: catch transfers + memos . using transfer = eosio::currency::transfer;
+    void apply(account_name contract, account_name act) {
+
+        if( act == N(transfer) ) {
+            _transfer(unpack_action_data<currency::transfer>(), contract);
+            return;
+        }
+
+        auto &thiscontract = *this;
+        switch (act) {
+            EOSIO_API(twitbot, (tip)(withdraw)(claim))
+        };
+    }
+
+    void _transfer(const currency::transfer& trs, account_name code) {
+        print("transfer received");
+    }
 
 private:
 
@@ -33,7 +56,7 @@ private:
         string twitter;
         uint64_t balance = 0;
 
-        uint64_t primary_key()const { return name; }
+        uint64_t primary_key() const { return name; }
 
         static key256 key(string twitter) {
             return key256::make_from_word_sequence<uint64_t>(string_to_name(twitter.c_str()));
@@ -41,7 +64,7 @@ private:
 
         key256 get_key() const { return key(twitter); }
 
-        EOSLIB_SERIALIZE( account, (name)(twitter)(balance))
+        EOSLIB_SERIALIZE(account, (name)(twitter)(balance))
     };
 
     typedef eosio::multi_index<N(account), account,
@@ -49,4 +72,11 @@ private:
     > account_index_type;
 };
 
-EOSIO_ABI(twitbot, (tip)(withdraw)(claim))
+extern "C" {
+    [[noreturn]] void apply(uint64_t receiver, uint64_t code, uint64_t action) {
+        auto self = receiver;
+        twitbot twit(self);
+        twit.apply(code, action);
+        eosio_exit(0);
+    }
+}
